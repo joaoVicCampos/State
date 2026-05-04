14008bca37053d9826bd2b6c71e00ba90da907f4
joaoVicCampos <ripjv1234@gmail.com>
2026-05-04 15:12:57 -0300

    prepara estrutura do projeto

---
+++ src/main/java/org/padroes/Main.java
@@ -0,0 +1,8 @@
+package org.padroes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Demo do padrao State: Chamado Tecnico");
+        System.out.println("Implementacao sera concluida em etapas para commits pequenos.");
+    }
+}
